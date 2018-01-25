9323df7d651b8bfcf766576f807d0e61b5c62b2b
Hemant <patidarhemant27@gmail.com>
2018-01-25 07:15:59 +0000

    Added few files for blob upload

--- AccessAzureStorage/Controllers/BlobController.cs
+++ AccessAzureStorage/Controllers/BlobController.cs
@@ -3,15 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Configuration;
 
 namespace AccessAzureStorage.Controllers
 {
     public class BlobController : Controller
     {
-        // GET: Blob
+        private static CloudStorageAccount CloudStorageAccount = 
+            CloudStorageAccount.Parse(ConfigurationManager.AppSettings.Get("StorageConnectionString"));
+
+        private static CloudBlobClient Client = CloudStorageAccount.CreateCloudBlobClient();
+        
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Upload()
+        {
+            try
+            {
+                CloudBlobContainer Container = Client.GetContainerReference("file-container");
+                Container.CreateIfNotExists();
+                string FileName = DateTime.Now.ToString() + ".txt";
+
+                CloudBlockBlob Blob = Container.GetBlockBlobReference(FileName);
+                Blob.UploadText("File written on " + DateTime.Now.ToString());
+            }
+            catch (Exception)
+            {
+                return View("Error");
+            }
+            return View("Success");
+        }
+
+        public ActionResult Download()
+        {
+            return View("Success");
         }
     }
 }
